47e3c8fd34cc30baae2129a07ce55c87e850d719
Samuel <samuelperron@hotmail.fr>
2021-11-25 00:28:31 -0500

    backtests - Invest

--- src/backtests/backtest_bot.py
+++ src/backtests/backtest_bot.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import re
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from typing import Optional
 from src.strategies import Strategy
@@ -52,7 +53,9 @@ class BacktestBot:
         self.money_added_at_interval = money_added_at_interval
 
         self.df = self.load_df()
-        print(self.df)
+
+        self.df.plot(y=['available_capital', 'c'], kind = 'line')
+        plt.show()
 
     @staticmethod
     def compute_interval_to_timedelta(interval: str) -> timedelta:
@@ -104,8 +107,11 @@ class BacktestBot:
         df = self._add_invested_capital(df)
 
         # Add buy and sell signals
-        df = self._add_buy_signals(df)
-        df = self._add_sell_signals(df)
+        df['buy'] = df.apply(lambda row: self.strategy.entry_signal(row), axis=1)
+        df['sell'] = df.apply(lambda row: self.strategy.exit_signal(row), axis=1)
+
+        # Add portfolio investments
+        df = self._invest(df)
 
         return df
 
@@ -119,11 +125,11 @@ class BacktestBot:
             return df
 
         def calculate_invested_capital(invested_capital: list, movement: list) -> list:
-            res = np.empty(invested_capital.shape)
-            res[0] = invested_capital[0]
+            result = np.empty(invested_capital.shape)
+            result[0] = invested_capital[0]
             for i in range(1, invested_capital.shape[0]):
-                res[i] = res[i-1] + float(movement[i])
-            return res
+                result[i] = result[i-1] + float(movement[i])
+            return result
 
         dates = self.get_dates_from_intervals(start, end)
         df.loc[df.index.isin(dates), 'invested_capital_movement'] = self.money_added_at_interval
@@ -135,12 +141,39 @@ class BacktestBot:
 
         return df
 
-    def _add_buy_signals(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['buy'] = df.apply(lambda row: self.strategy.entry_signal(row), axis=1)
-        return df
+    def _invest(self, df):
+        def get_available_capital(capital, price, buy, sell) -> list:
+            result = np.empty(capital.shape)
+            result[0] = capital[0]
+            quantities = []
+            for i in range(1, capital.shape[0]):
+                if buy[i]:
+                    qty = self.strategy.find_qty(price[i], result[i-1])
+                    if qty != 0:
+                        quantities.append(qty)
+                        price_of_buy = quantities[-1] * price[i]
+                        result[i] = result[i-1] - price_of_buy
+                    
+                    else:
+                        result[i] = result[i-1]
 
-    def _add_sell_signals(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['exit'] = df.apply(lambda row: self.strategy.exit_signal(row), axis=1)
+                elif sell[i] and len(quantities) != 0:
+                    roi = sum(quantities) * price[i]
+                    result[i] = result[i-1] + roi
+                    quantities = []
+
+                else:
+                    result[i] = result[i-1]
+
+            return result
+
+        df.loc[df.index[0], 'available_capital'] = self.starting_capital
+        df['available_capital'] = get_available_capital(
+            df['available_capital'].values.T,
+            df['c'].values.T,
+            df['buy'].values.T,
+            df['sell'].values.T,
+        )
         return df
 
     def get_dates_from_intervals(self, start: datetime, end: datetime) -> list:
